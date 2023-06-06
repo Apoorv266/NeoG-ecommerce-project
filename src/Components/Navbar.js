83cec7ce4e08313520b97a98220fe6a414eed86c
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-06-06 20:56:54 +0530

    style : logo

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,70 +1,67 @@
-import React, { useContext } from 'react'
-import "../Styles/Navbar.css"
-import { Link, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../Contexts/Auth';
-import { productContext } from '../Contexts/ProductContext';
-import { LogInOutline, HomeOutline, HeartOutline, CartOutline,PersonOutline } from "react-ionicons";
+import React, { useContext } from "react";
+import "../Styles/Navbar.css";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../Contexts/Auth";
+import { productContext } from "../Contexts/ProductContext";
+import {
+  LogInOutline,
+  HomeOutline,
+  HeartOutline,
+  CartOutline,
+  PersonOutline,
+} from "react-ionicons";
 const Navbar = () => {
   const { token } = useContext(AuthContext);
-  const { filterDispatch, filterState } = useContext(productContext)
-  const navigate = useNavigate()
+  const { filterDispatch, filterState } = useContext(productContext);
+  const navigate = useNavigate();
   return (
-    <div className='nav-main'>
+    <div className="nav-main">
       <Link to={"/"}>
-      <span>LOGO</span>
+        <img
+          src={require("../Images/logo.png")}
+          alt=""
+          srcSet=""
+          id="logo-img"
+        />
       </Link>
       <div>
-        <input type='text' placeholder='Search product here...' value={filterState.searchTxt} onChange={(event) => {
-          filterDispatch({ type: "ADD_INPUT_FIELD", payload: event.target.value });
-          navigate("/products");
-        }} />
+        <input
+          type="text"
+          placeholder="Search product here..."
+          value={filterState.searchTxt}
+          onChange={(event) => {
+            filterDispatch({
+              type: "ADD_INPUT_FIELD",
+              payload: event.target.value,
+            });
+            navigate("/products");
+          }}
+        />
       </div>
-      <div className='icons'>
+      <div className="icons">
         <Link to={"/products"}>
-          <HomeOutline
-            color={'#ffffff'}
-            height="20px"
-            width="20px"
-          />
+          <HomeOutline color={"#ffffff"} height="20px" width="20px" />
         </Link>
         <Link to={"/wishlist"}>
-          <HeartOutline
-            color={'#ffffff'}
-            height="20px"
-            width="20px"
-          />
+          <HeartOutline color={"#ffffff"} height="20px" width="20px" />
         </Link>
-
 
         <Link to={"/cart"}>
-          <CartOutline
-            color={'#ffffff'}
-            height="20px"
-            width="20px"
-          />
+          <CartOutline color={"#ffffff"} height="20px" width="20px" />
         </Link>
 
-
-        {token ? <Link to={"/user-details"}>
-          <PersonOutline
-  color={'#ffffff'} 
-  height="20px"
-  width="20px"
-/>
-        </Link>
-          :
+        {token ? (
+          <Link to={"/user-details"}>
+            <PersonOutline color={"#ffffff"} height="20px" width="20px" />
+          </Link>
+        ) : (
           <Link to={"/login"}>
-            <LogInOutline
-              color={'white'}
-              height="20px"
-              width="20px"
-            />
-          </Link>}
-
-
+            <LogInOutline color={"white"} height="20px" width="20px" />
+          </Link>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
